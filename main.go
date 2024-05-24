9b454883fc438e7d65d4fd6805eafd0073309105
alex <apryakhin@rzdit.ru>
2024-05-24 17:24:51 +0300

    feat: add some middleware

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	handlers.InitHandlers(r)
 
 	err := http.ListenAndServe(":5556", r)
